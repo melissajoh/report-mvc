5bf69ed7381617fdc073012e46977fefc362b58c
Melissa Johansson <mejo22@student.bth.se>
2023-03-30 16:57:43 +0200

    Updated quotes

--- src/Controller/JsonController.php
+++ src/Controller/JsonController.php
@@ -11,9 +11,9 @@ class JsonController
     #[Route("/api/quote", name: "quote")]
     public function jsonNumber(): Response
     {
-        $quotes = array("When life throws you a rainy day, play in the puddles", 
+        $quotes = array("When life throws you a rainy day, play in the puddles.", 
         "Whatever you do, always give 100%. Unless you are donating blood.", 
-        "If you think you are too small to make a difference, try sleeping with a mosquito.");
+        "If you think you are too small to make a difference, try sleeping with a mosquito in the room.");
         $number = random_int(0, count($quotes)-1);
         $date = date('d-m-Y');
         $timestamp = time();
