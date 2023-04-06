83ef753c3d8dd8c15319b6d100d6f5ddb5ee0686
Melissa Johansson <mejo22@student.bth.se>
2023-04-06 13:25:15 +0200

    Add route for /api and remove commented out code

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -39,27 +39,9 @@ class MainController extends AbstractController
         return $this->render('lucky.html.twig', $data);
     }
 
-    // #[Route("/api/quote", name: "quote")]
-    // public function jsonNumber(): Response
-    // {
-    //     $quotes = array("When life throws you a rainy day, play in the puddles",
-    //     "Whatever you do, always give 100%. Unless you are donating blood.",
-    //     "If you think you are too small to make a difference, try sleeping
-    //     with a mosquito.");
-    //     $number = random_int(0, count($quotes)-1);
-    //     $date = date('d-m-Y');
-    //     $timestamp = time();
-
-    //     $data = [
-    //         'quote' => $quotes[$number],
-    //         'date' => $date,
-    //         'timestamp' => $timestamp,
-    //     ];
-
-    //     $response = new JsonResponse($data);
-    //     $response->setEncodingOptions(
-    //         $response->getEncodingOptions() | JSON_PRETTY_PRINT
-    //     );
-    //     return $response;
-    // }
+    #[Route("/api", name: "api")]
+    public function api(): Response
+    {
+        return $this->render('api.html.twig');
+    }
 }
