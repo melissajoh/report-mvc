b8e343e3b27243610197ce08544e53046bab719c
Melissa Johansson <mejo22@student.bth.se>
2023-04-20 15:45:02 +0200

    Add game functionality for statistics, smarter bank

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -26,15 +26,30 @@ class GameController extends AbstractController
         $session->set("flash", null);
         $session->set("bank_won", 0);
         $session->set("player_won", 0);
+        $session->set("bank_currency", $game->getBank()->getCurrency());
+        $session->set("player_currency", $game->getPlayer()->getCurrency());
+        $session->set("closeGame", false);
         return $this->render('game/home.html.twig');
     }
 
     #[Route("/game/doc", name: "game_doc")]
-    public function doc(
+    public function doc(): Response
+    {
+        return $this->render('game/doc.html.twig');
+    }
+
+    #[Route("/game/init", name: "game_init", methods: ['POST', 'GET'])]
+    public function init(
+        Request $request,
         SessionInterface $session
     ): Response {
+        $session->set("flash", null);
+        $bet = $request->request->get('bet');
 
-        return $this->render('game/doc.html.twig');
+        $session->set("bet", $bet);
+        $session->set("which_bank", $request->request->get("bank"));
+
+        return $this->redirectToRoute('game_play');
     }
 
     #[Route("/game/play", name: "game_play")]
@@ -45,11 +60,20 @@ class GameController extends AbstractController
             $game = new GameBoard();
             $session->set("game", $game);
         }
+
+        /**
+         * @var GameBoard $game Current game of 21
+         */
         $game = $session->get("game");
 
-        // var_dump($game);
-        // var_dump($game->getHand()->getHandString());
+        /**
+         * @var int Bet
+         */
+        $bet = $session->get("bet");
 
+        /**
+         * Score and rounds won
+         */
         $playerScore = $game->getPlayer()->getScore();
         $session->set("player_score", $playerScore);
 
@@ -59,39 +83,42 @@ class GameController extends AbstractController
         $playerWon = $session->get("player_won");
         $bankWon = $session->get("bank_won");
 
+        /**
+         * Flash messages and dealing with currency
+         */
         if ($playerScore > 21) {
-            $session->set("flash", 'Du fick över 21. Banken vann denna runda!');
+            $session->set("flash", 'Banken vann denna runda!');
+        } elseif ($playerScore === 21) {
+            $session->set("flash", 'Spelaren vann denna runda!');
+        }
+
+        if ($session->get("flash") == "Banken vann denna runda!") {
             $this->addFlash(
                 'lost',
-                'Du fick över 21. Banken vann denna runda!'
+                $session->get("flash")
             );
             $session->set("bank_won", $bankWon +1);
-        } elseif ($playerScore === 21) {
-            $session->set("flash", 'Du fick 21. Spelaren vann denna runda!');
+            $session->set("bank_currency", $game->getBank()->addCurrency($bet));
+            $session->set("player_currency", $game->getPlayer()->removeCurrency($bet));
+        } elseif ($session->get("flash") == "Spelaren vann denna runda!") {
             $this->addFlash(
                 'won',
-                'Du fick 21. Spelaren vann denna runda!'
+                $session->get("flash")
             );
             $session->set("player_won", $playerWon +1);
+            $session->set("bank_currency", $game->getBank()->removeCurrency($bet));
+            $session->set("player_currency", $game->getPlayer()->addCurrency($bet));
         }
 
-        if ($session->get("flash") != null) {
-            if ($session->get("flash") == "Banken vann denna runda!") {
-                $this->addFlash(
-                    'lost',
-                    $session->get("flash")
-                );
-                $session->set("bank_won", $bankWon +1);
-            } elseif ($session->get("flash") == "Spelaren vann denna runda!") {
-                $this->addFlash(
-                    'won',
-                    $session->get("flash")
-                );
-                $session->set("player_won", $playerWon +1);
-            }
-        }
+        /**
+         * Checking if deck is empty or someone is out of currency, end game
+         */
+        $playerCurrency = $session->get("player_currency");
+        $bankCurrency = $session->get("bank_currency");
 
-        $session->set("hand", $game->getHand()->getHandString());
+        if ($game->checkEmptyDeck() == true or $playerCurrency == 0 or $bankCurrency == 0) {
+            $session->set("closeGame", true);
+        }
 
         $data = [
             "card" => $game->getDrawnCard(),
@@ -99,8 +126,12 @@ class GameController extends AbstractController
             "player" => $session->get("player_score"),
             "bank" => $session->get("bank_score"),
             "flash" => $session->get("flash"),
-            "player_won" => $session->get("player_won"),
-            "bank_won" => $session->get("bank_won")
+            "playerWon" => $session->get("player_won"),
+            "bankWon" => $session->get("bank_won"),
+            "playerCurrency" => $session->get("player_currency"),
+            "bankCurrency" => $session->get("bank_currency"),
+            "statistics" => $game->getStatistics($playerScore),
+            "closeGame" => $session->get("closeGame")
         ];
 
         return $this->render('game/play.html.twig', $data);
@@ -110,9 +141,17 @@ class GameController extends AbstractController
     public function draw(
         SessionInterface $session
     ): Response {
+        /**
+         * @var GameBoard $game Current game of 21
+         */
         $game = $session->get("game");
-        $game->playerDraws();
-        $session->set("game", $game);
+
+        if ($game->checkEmptyDeck() != true) {
+            $game->playerDraws();
+            $session->set("game", $game);
+        }
+
+        $session->set("flash", null);
 
         return $this->redirectToRoute('game_play');
     }
@@ -121,13 +160,21 @@ class GameController extends AbstractController
     public function save(
         SessionInterface $session
     ): Response {
-        $playerScore = $session->get("player_score");
-
+        /**
+         * @var GameBoard $game Current game of 21
+         */
         $game = $session->get("game");
-        $game->getHand()->clearHand();
+        if ($game->getHand() != null) {
+            $game->getHand()->clearHand();
+        }
 
-        $bankScore = $game->bankDraws();
-        $session->set("bank_score", $bankScore);
+        if ($session->get("which_bank") == "smart") {
+            $bankScore = $game->bankDrawsSmarter();
+            $session->set("bank_score", $bankScore);
+        } elseif ($session->get("which_bank") == "dumb") {
+            $bankScore = $game->bankDraws();
+            $session->set("bank_score", $bankScore);
+        }
 
         $session->set("flash", $game->endGame());
 
@@ -138,7 +185,11 @@ class GameController extends AbstractController
     public function rematch(
         SessionInterface $session
     ): Response {
+        /**
+         * @var GameBoard $game Current game of 21
+         */
         $game = $session->get("game");
+
         $session->set("player_score", $game->getPlayer()->resetScore());
         $session->set("bank_score", $game->getBank()->resetScore());
         $session->set("hand", $game->getHand()->clearHand());
