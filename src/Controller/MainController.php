0584ac953bc88dacefc8495402cc9671e3a0fa4e
Melissa Johansson <mejo22@student.bth.se>
2023-05-11 17:44:57 +0200

    Add metrics route

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -47,4 +47,10 @@ class MainController extends AbstractController
     {
         return $this->render('api.html.twig');
     }
+
+    #[Route("/metrics", name: "metrics")]
+    public function metrics(): Response
+    {
+        return $this->render('metrics.html.twig');
+    }
 }
