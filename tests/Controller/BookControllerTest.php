b2207b1f17578cf03f637f77a72f8e4627497722
Melissa Johansson <mejo22@student.bth.se>
2023-05-12 07:47:21 +0200

    Add Book tests

---
+++ tests/Controller/BookControllerTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Test cases for controller BookController
+ */
+class BookControllerTest extends WebTestCase
+{
+    public function testLibraryRoute(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/library');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testLibraryShowRoute(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/library/show');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    // public function testLibraryShowIdRoute(): void
+    // {
+    //     $client = static::createClient();
+
+    //     $client->request('GET', '/library/{id}');
+
+    //     $this->assertResponseIsSuccessful();
+    // }
+
+    public function testLibraryCreate(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/library/create');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testLibraryBooksRoute(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', 'api/library/books');
+
+        $this->assertResponseIsSuccessful();
+    }
+}
